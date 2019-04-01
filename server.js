86bf2ce5f0bae263f1ec2421cb79f85c6cd4c0a8
jesper <jesper_truedsson@hotmail.com>
2019-04-01 20:31:53 +0200

    Now i get a random theme on the random-theme route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:admin1@ds145053.mlab.com:45053/samtaldb', {useNewUrlParser: true});
+mongoose.connect('mongodb://admin:admin1@ds213615.mlab.com:13615/samtalsdb', {useNewUrlParser: true});
 
 
 // Varible for all the models / collections.
@@ -33,13 +33,23 @@ var UserSchema = new Schema ({
 var User = mongoose.model("User", UserSchema);
 
 
+
 // Theme Schema.
-var ThemeSchema = new Schema ({});
+var ThemeSchema = new Schema ({
+  themeobj: {
+    type: String
+  }
+});
 var Theme = mongoose.model("Theme", ThemeSchema);
 
 
+
 // Question Schema.
-var QuestionSchema = new Schema ({});
+var QuestionSchema = new Schema ({
+  questionobj: {
+    type: String
+  }
+});
 var Question = mongoose.model("Question", QuestionSchema);
 
 
@@ -64,20 +74,25 @@ app.use(function(req, res, next) {
 
 
 // Test route.
+/*
 app.get("/test", function(req, res) {
 
   function hej() {
-    var data = 23
+    
+    Theme.find({}).then(function(data) {
+      var json = data;
 
-    if ( data > 30) {
-      console.log("Data is " + data + " and is bigger.")
-    } else {
-      console.log("Data is " + data + " and is not bigger.")
-    }
+      var randomTheme = json[Math.floor(Math.random() * json.length)];
+      
+      
+      console.log(randomTheme);
+    });
+
   }
   hej()
 
 });
+*/
 
 
 
@@ -85,13 +100,23 @@ app.get("/test", function(req, res) {
 
 
 
-
-// Root route.
+// Welcome route.
 app.get("/", function(req, res) {
   res.send("Hello World from the server")
 });
 
 
+// Shows all users in json, this is for admin. 
+app.get("/users", function(req, res) {
+
+  User.find({}).then(function(data) {
+    res.json(data);
+  });
+
+});
+
+
+// Post a new user object to database.
 app.post("/register", function(req, res) {
   var user = new User(req.body); //This creates a new object in the Userschema.
 
@@ -100,13 +125,13 @@ app.post("/register", function(req, res) {
     res.json(data);
   });
 
-
 });
 
-
+// Delete a existing user object with it's id. 
 app.delete("/delete/user", function(req, res) {
   var id = req.body.id;
 
+  // findOneAndRemove() is function that mongoose uses. It is not standard javascript!
   User.findOneAndRemove({_id: id}, function(req, res) {
     if(!err) {
       res.send("Deleted user with id:" + id + "successfully.");
@@ -118,25 +143,114 @@ app.delete("/delete/user", function(req, res) {
 });
 
 
-//app.post("/login", function(req, res) {});
-
-//app.get("/user", function(req, res) {});
 
 
-app.get("/users", function(req, res) {
 
-  User.find({}).then(function(data) {
+
+// Randomize a random string from json array and send it back.
+app.get("/theme/random-theme", function(req, res) {
+
+  function Random() {
+    Theme.find({}).then(function(data) {
+      var json = data;
+      var randomTheme = json[Math.floor(Math.random() * json.length)];
+      res.send(randomTheme.themeobj);
+    });
+  }
+  Random()
+
+});
+
+// Shows all the themes, this is for admin.
+app.get("/theme", function(req, res) {
+
+  Theme.find({}).then(function(data) {
     res.json(data);
   });
 
 });
 
-/*
-app.get("/:writeYourParamsHere", function(req, res) {
-  res.send(req.params.writeYourParamsHere);
-});
-*/
+// Post a new theme object to database.
+app.post("/theme/post", function(req, res) {
+  var theme = new Theme(req.body); //This creates a new object in the Themeschema.
 
+  // Saves the new object to the mongodb database.
+  theme.save( function(error, data) {
+    res.json(data);
+  });
+
+});
+
+
+app.delete("/theme/delete", function(req, res) {
+  var id = req.body.id;
+
+  // findOneAndRemove() is function that mongoose uses. It is not standard javascript!
+  Theme.findOneAndRemove({_id: id}, function(req, res) {
+    if(!err) {
+      res.send("Theme with: " + id + " successfully.");
+    } else {
+      res.send(err);
+    }
+
+  });
+});
+
+
+
+
+
+
+// Randomize a random string from json array and send it back.
+app.get("/question/random-question", function(req, res) {
+
+  function Random() {
+    Theme.find({}).then(function(data) {
+      var json = data;
+      var randomQuestion = json[Math.floor(Math.random() * json.length)];
+      res.send(randomQuestion.questionobj);
+    });
+  }
+  Random()
+
+});
+
+
+// Shows all the questions, this is for admin.
+app.get("/theme", function(req, res) {
+
+  Question.find({}).then(function(data) {
+    res.json(data);
+  });
+
+});
+
+// Post a new theme object to database.
+app.post("/question/post", function(req, res) {
+  var question = new Question(req.body); //This creates a new object in the Questionschema.
+
+  // Saves the new object to the mongodb database.
+  question.save( function(error, data) {
+    res.json(data);
+  });
+
+
+});
+
+
+app.delete("/question/delete", function(req, res) {
+  var id = req.body.id;
+
+  // findOneAndRemove() is function that mongoose uses. It is not standard javascript!
+  Theme.findOneAndRemove({_id: id}, function(req, res) {
+    if(!err) {
+      res.send("Theme with: " + id + " successfully.");
+    } else {
+      res.send(err);
+    }
+
+  });
+});
 
 
 // Listen to process environment port, or port 3000.
